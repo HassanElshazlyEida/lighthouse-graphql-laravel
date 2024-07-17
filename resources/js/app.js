19c4df3ada2e442d5ce3a68bbfc355e00295c318
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-07-18 02:43:28 +0300

    💥 NEW: add vuejs Vite & upgrade to Laravel 9

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+// Create a Vue application instance
+const app = createApp({});
+
+// Register a Vue component
+app.component('example-component', ExampleComponent);
+
+// Mount the Vue application to an HTML element with the ID 'app'
+app.mount('#app');
