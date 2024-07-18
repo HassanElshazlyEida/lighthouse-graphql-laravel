9861f64839fd574445cce3674176c3511da22094
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-07-18 16:20:56 +0300

    🐛 FIX: replace const url with vite url

--- resources/js/apollo.js
+++ resources/js/apollo.js
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
 import { createApolloProvider } from '@vue/apollo-option';
 
+
 // HTTP connection to the API
 const httpLink = new HttpLink({
   // You should use an absolute URL here
-  uri: 'http://127.0.0.1:8000/graphql',
+  uri:`${import.meta.env.VITE_APP_URL}/graphql`,
 });
 
 // Cache implementation
