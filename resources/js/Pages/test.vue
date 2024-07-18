6dd8cbd31b1bd50056163f514a6cbb6eceff68f1
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-07-18 17:08:28 +0300

    💥 NEW: vue apollo mutation

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
       <h1>te to Inertia.js with Vue 3</h1>
-      <div v-for="user in users.data" :key="user.name">
-      {{ user.name }}
-    </div>
+        <div v-if="users && users.data">
+            <div v-for="user in users.data" :key="user.name">
+            {{ user.name }}
+            </div>
+        </div>
+        <p> latest post</p>
+        <p v-if="latestPost"> {{  latestPost.content }} </p>
+        <button @click="handleClick"> create User mutation</button>
     </div>
   </template>
   
@@ -23,6 +28,30 @@
         }
       `,
     },
-  },
+    latestPost : {
+        query: gql`
+        query {
+            latestPost {
+                content
+            }
+        }
+        `,
+    }
+    },
+    methods: {
+        handleClick() {
+            this.$apollo.mutate({
+                mutation: gql`
+                mutation {
+                    createUser(name:"hasona",email:"has2222@gmail.com",password:"password") {
+                        id,
+                        name,
+                        email
+                    }
+                }   
+                `,
+            });
+        },
+    },
   };
   </script>
