b3f016aaa9713d99343de36913f6dd4a4e31b9d6
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-07-22 19:26:19 +0300

    💥 NEW: php client

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Softonic\GraphQL\ClientBuilder;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,40 @@ Route::get('/books/create', function () {
 Route::get('/books/{id}', function ($id) {
     return Inertia::render('Book', ['bookId' => $id]);
 })->name('books.show');
+
+Route::get("test",function(){
+    // guzzle client
+    $client = new \GuzzleHttp\Client();
+    $response = $client->post(env('APP_URL').'/graphql', [
+        'json' => [
+            'query' => 'query {
+                books {
+                    id
+                    title
+                    author
+                    category {
+                        id
+                        name
+                    }
+                }
+            }'
+        ]
+    ]);
+    $response = $response->getBody()->getContents();
+    dd(json_decode($response));
+    // Softonic client builder 
+    $client = ClientBuilder::build(env('APP_URL').'/graphql');
+    $query = 'query {
+        books {
+            id
+            title
+            author
+            category {
+                id
+                name
+            }
+        }
+    }';
+    $response = $client->query($query);
+    dd($response->getData());
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
